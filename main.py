d91744463e7afeee488fb9ed8c1c4bfb63d4e8d2
Stepaka <55946338+Stepaka@users.noreply.github.com>
2021-02-16 16:34:55 +0300

    Code style fixed!

--- main.py
+++ main.py
@@ -1,79 +1,101 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import math
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.fft import rfft, rfftfreq
 
 
-class InfoOs:
-    def __init__(self, init_info):
-        init_info = init_info.split('\n')
-        LineChannels=init_info[1].split(',')
-        #self.AllChannels = LineChannels[0]
-        self.channelsA = int(LineChannels[1][:-1])
-        self.channelsD = int(LineChannels[2][:-1])
-        self.size_line_data = (self.channelsA + math.ceil(self.channelsD/16))*2
-        self.coffTransforList = []
-        self.formats = []
-        self.formats.append(np.uint32)
-        self.formats.append(np.uint32)
-        for i in range(self.size_line_data):
-            self.formats.append(np.int16)
-        self.name = []
-        self.name.append('Nomber')
-        self.name.append('time')
-        for i in range(2, self.channelsA+2):
-            temp = init_info[i].split(',')
-            self.coffTransforList.append(float(temp[-2]))
-            LineChannels=init_info[i].split(',')
-            self.name.append(LineChannels[1])
-        for i in range(math.ceil(self.channelsD/16)):
-            self.name.append('D'+str(i+1))
-            #print(self.name)
-        self.coffTransfor = dict(zip(self.name[2:], self.coffTransforList))
-        self.simpleRate = int(init_info[self.channelsA+self.channelsD+4].split(',')[0])
+FILE_DAT_NAME = "24e61716a082f03660813bc0fd6b3f56458cac13.dat"
+FILE_CFG_NAME = "24e61716a082f03660813bc0fd6b3f56458cac13.cfg"
 
-def get_info_from_file(filename):
+
+class AnalyzerRPA:
+    def __init__(self, init_data):
+        init_data = init_data.split('\n')
+
+        # Получение информации о каналах.
+        channels_info = init_data[1].split(',')
+        self.channels_A = int(channels_info[1][:-1])    # Количество аналоговых каналов.
+        self.channels_D = int(channels_info[2][:-1])    # Количество дискретных каналов.
+
+        # Вычисление размера строки одной выборки, без учета ее номера и времени.
+        self.data_line_size = (2 * (self.channels_A + math.ceil(self.channels_D / 16)))
+
+        # Формат бинарных данных в одной строке.
+        self.list_format = []
+        self.list_format.append(np.uint32)   # Номер выборки.
+        self.list_format.append(np.uint32)   # Время выборки.
+
+        # Добавление формата для значений каналов.
+        for i in range(self.data_line_size):
+            self.list_format.append(np.int16)
+
+        # Список коэффициентов трансформации аналоговый каналов.
+        self.transform_coef_list = []
+
+        # Названия столбцов для DataFrame.
+        self.cols_names = []
+        self.cols_names.append('Number')
+        self.cols_names.append('Time')
+
+        # Добавление названий столбцов аналоговых каналов для DataFrame.
+        for i in range(self.channels_A):
+            temp = init_data[i + 2].split(',')
+            self.cols_names.append(temp[1])
+
+            # Добавление коэффициента трансформации текущего аналогового канала.
+            self.transform_coef_list.append(float(temp[-2]))
+
+        # Добавление названий столбцов дискретных каналов для DataFrame.
+        for i in range(math.ceil(self.channels_D / 16)):
+            self.cols_names.append('D' + str(i + 1))
+
+        # Частота дискретизации.
+        self.simple_rate = int(init_data[self.channels_A + self.channels_D + 4].split(',')[0])
+
+        # Преобразование списка коэффициентов трансформации в словарь ("имя" -> коэфф. трансформации).
+        self.transform_coef_dict = dict(zip(self.cols_names[2:], self.transform_coef_list))
+
+        # Создание DataFrame на основе полученных данных.
+        temp_data = np.fromfile(FILE_DAT_NAME,
+                                dtype=np.dtype({'names': self.cols_names,
+                                                'formats': self.list_format}))
+        self.data_frame = pd.DataFrame(temp_data, columns=temp_data.dtype.names)
+
+
+def get_data_from_file(filename):
     with open(filename, "r") as f:
-        info =f.read()
-    return info
-
+        content = f.read()
+    return content
 
 
 if __name__ == '__main__':
-    input_info =  get_info_from_file("24e61716a082f03660813bc0fd6b3f56458cac13.cfg")
-    constLineSize = InfoOs(input_info)
-    dt = np.dtype({'names': constLineSize.name,
-               'formats': constLineSize.formats})
-    data = np.fromfile("24e61716a082f03660813bc0fd6b3f56458cac13.dat", dtype=dt)
-    df = pd.DataFrame(data)
-    df = pd.DataFrame(data, columns=data.dtype.names)
+    input_data = get_data_from_file(FILE_CFG_NAME)
+    analyzer = AnalyzerRPA(input_data)
 
-    #Задаем интервал на котором происходит преобразования Фурье
-    df=df.loc[5000:10000]
+    # Задаем интервал, на котором происходят преобразования Фурье.
+    df = analyzer.data_frame.loc[0:35]
+
     print(df)
 
-    name_graf_to_print = ['Ix2']#'Ix1','Ix2','Ix3'
-    timePoint = df['time'].tolist()
-    graf_values=[[] for i in range(len(name_graf_to_print))]
-    for i in range(len(name_graf_to_print)):
-        graf_values[i] =  df[name_graf_to_print[i]].tolist()
+    cols_names_to_print = ['Ix1']
+    timePoint = df['Time'].tolist()
+    graf_values=[[] for i in range(len(cols_names_to_print))]
+    for i in range(len(cols_names_to_print)):
+        graf_values[i] =  df[cols_names_to_print[i]].tolist()
         for el, item in enumerate(graf_values[i]):
-            graf_values[i][el] = float(item)/constLineSize.coffTransfor[name_graf_to_print[i]]
-        plt.plot(timePoint, graf_values[i], label=name_graf_to_print[i])
+            graf_values[i][el] = float(item)/analyzer.transform_coef_dict[cols_names_to_print[i]]
+        plt.plot(timePoint, graf_values[i], label=cols_names_to_print[i])
     plt.legend(loc=2)
     plt.show()
 
-    #Блок Фурье
+    # Блок Фурье.
     myarray = np.asarray((graf_values[0]))
-    normalized_tone = np.int16((myarray / myarray.max()) * 32767)
-    # число точек в normalized_tone
+
+    # Число точек в normalized_tone.
     N = len(myarray)
-    yf = rfft(normalized_tone)
+    yf = rfft(myarray)
     print(len(yf))
-    xf = rfftfreq(N, 1 / constLineSize.simpleRate)
+    xf = rfftfreq(N, 1 / analyzer.simple_rate)
     plt.plot(xf, np.abs(yf))
     plt.show()
-
-    #plt.xlim (0, 5000)
-    #print(df['time'][:5])
